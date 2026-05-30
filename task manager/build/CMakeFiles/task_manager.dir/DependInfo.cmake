d2ec8d254081a11f71070016cc0d295ef10d5563
kaloyan-v-dimitrov <kaloyan.v.dimitrov.2023@elsys-bg.org>
2026-05-30 20:21:58 +0000

    fix file structure

---
+++ task manager/build/CMakeFiles/task_manager.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/OOP-project/task manager/src/TaskManager.cpp" "CMakeFiles/task_manager.dir/src/TaskManager.cpp.o" "gcc" "CMakeFiles/task_manager.dir/src/TaskManager.cpp.o.d"
+  "/workspaces/OOP-project/task manager/src/Trackable.cpp" "CMakeFiles/task_manager.dir/src/Trackable.cpp.o" "gcc" "CMakeFiles/task_manager.dir/src/Trackable.cpp.o.d"
+  "/workspaces/OOP-project/task manager/src/main.cpp" "CMakeFiles/task_manager.dir/src/main.cpp.o" "gcc" "CMakeFiles/task_manager.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ task manager/build/CMakeFiles/task_manager.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_manager.dir/src/TaskManager.cpp.o"
+  "CMakeFiles/task_manager.dir/src/TaskManager.cpp.o.d"
+  "CMakeFiles/task_manager.dir/src/Trackable.cpp.o"
+  "CMakeFiles/task_manager.dir/src/Trackable.cpp.o.d"
+  "CMakeFiles/task_manager.dir/src/main.cpp.o"
+  "CMakeFiles/task_manager.dir/src/main.cpp.o.d"
+  "task_manager"
+  "task_manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
